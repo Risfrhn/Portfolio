ee1038790ec7413010e48909b59f4a892ca33c6c
Muhammad Risky Farhan <rskyfrhn801@gmail.com>
2026-02-22 15:32:54 +0800

    update: bug error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceRootUrl(config('app.url'));
+        if (config('app.url')) {
+            URL::forceRootUrl(config('app.url'));
+        }
     }
 }
