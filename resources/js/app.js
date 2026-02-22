98e6e0c735a9de9574163eb6756fc8fe4efc56e4
Muhammad Risky Farhan <rskyfrhn801@gmail.com>
2026-02-22 15:08:08 +0800

    update: bug error

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,26 +4,28 @@ import './bootstrap';
 import Typed from 'typed.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-    const el = document.querySelector('.typed-text')
+  const el = document.querySelector('.typed-text')
 
-    if (el) {
-        const strings = JSON.parse(el.dataset.strings)
+  if (el) {
+    const strings = JSON.parse(el.dataset.strings)
 
-        new Typed(el, {
-            strings: strings,
-            typeSpeed: 80,
-            backSpeed: 50,
-            backDelay: 1500,
-            loop: true,
-            showCursor: false,
-        })
-    }
+    new Typed(el, {
+      strings: strings,
+      typeSpeed: 80,
+      backSpeed: 50,
+      backDelay: 1500,
+      loop: true,
+      showCursor: false,
+    })
+  }
 })
 
 
 // scroll navbar
 window.addEventListener("scroll", function () {
   const navbar = document.getElementById("navbar");
+
+  if (!navbar) return;
 
   if (window.scrollY > 50) {
     navbar.classList.add("scrolled");
