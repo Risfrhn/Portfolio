fb565195e70cc781fba9907cf5c04c5727b07589
Muhammad Risky Farhan <rskyfrhn801@gmail.com>
2026-02-24 13:13:58 +0800

    update: bug error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,17 +26,19 @@ class AppServiceProvider extends ServiceProvider
             URL::forceRootUrl(config('app.url'));
         }
 
-        // 1. Daftarkan route secara eksplisit (relatif terhadap sistem Laravel di subfolder)
+        $appUrl = rtrim(config('app.url'), '/');
+
+        // 1. Paksa URL script langsung ke file livewire.js (Full URL)
+        config(['livewire.asset_url' => $appUrl . '/livewire/livewire.js']);
+
+        // 2. Daftarkan route dengan prefix subfolder eksplisit
+        // Ini memastikan data-update-uri di HTML adalah "/app-portfolio/livewire/update"
         Livewire::setUpdateRoute(function ($handle) {
-            return Route::post('/livewire/update', $handle);
+            return Route::post('/app-portfolio/livewire/update', $handle);
         });
 
         Livewire::setScriptRoute(function ($handle) {
-            return Route::get('/livewire/livewire.js', $handle);
+            return Route::get('/app-portfolio/livewire/livewire.js', $handle);
         });
-
-        // 2. Paksa Asset URL menggunakan full URL (ditambah /livewire agar sinkron dengan route)
-        $appUrl = rtrim(config('app.url'), '/');
-        config(['livewire.asset_url' => $appUrl . '/livewire']);
     }
 }
